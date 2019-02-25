8f4e7495eed1a900f66b08e291e72ff5758c5d50
Wayne <quangvu7690@gmail.com>
2019-02-25 11:55:49 +0700

    push

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Video from './components/Video';
+import NavBar from './components/NavBar';
+import FeelingQuestion from './components/FeelingQuestion';
+import ListMusic from './components/ListMusic';
+import WhyReact from './components/WhyReact';
+import AboutUs from './components/AboutUs';
+import Bottom from './components/Bottom';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <Video/>
+        <NavBar/>
+        <FeelingQuestion/>
+        <ListMusic/>
+        <WhyReact/>
+        <AboutUs/>
+        <Bottom/>
       </div>
     );
   }
